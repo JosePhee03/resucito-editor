052566c0a9b28e84797374554e6ef0f19edcd0f0
José Luis Gutierrez <joselugz03@gmail.com>
2026-04-08 22:55:41 -0300

    format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,117 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [text, setText] = useState('')
+  const [hiddenJson, setHiddenJson] = useState(true)
+  const [jsonText, setJsonText] = useState<LyricNode[]>(parseText(text))
+  useEffect(() => {
+    setJsonText(parseText(text))
+  }, [text])
 
   return (
     <main className="w-screen h-screen flex p-8 gap-8 dark:bg-neutral-900 dark:text-white">
       <section className="flex flex-col gap-4 w-full h-full ">
-        <span className="font-bold dark:text-red-400">Escribir canto</span>
+        <span className="font-bold dark:text-red-400 my-auto h-8">
+          Escribir canto
+        </span>
         <textarea
-          className="w-full h-full p-4 font-mono dark:bg-neutral-800"
+          className="w-full h-full overflow-y-scroll p-4 font-mono dark:bg-neutral-800"
           onChange={e => setText(e.currentTarget.value)}
           placeholder="# Title"
         />
       </section>
       <section className="w-full h-full flex flex-col gap-4">
-        <span className="font-bold dark:text-red-400">Formato de Canto</span>
-        <ParseText text={text} />
+        <div className="flex justify-between items-center h-8">
+          <span className="font-bold dark:text-red-400">Formato de Canto</span>
+          <button
+            onClick={() => setHiddenJson(!hiddenJson)}
+            className="bg-red-400 text-white px-4 h-full "
+          >
+            {hiddenJson ? 'json' : 'formato'}
+          </button>
+        </div>
+        {hiddenJson ? (
+          <FormatView json={jsonText} />
+        ) : (
+          <JsonView json={jsonText} />
+        )}
       </section>
     </main>
   )
 }
 
-function ParseText({ text }: { text: string }) {
+function FormatView({ json }: { json: LyricNode[] }) {
   return (
-    <pre className="w-full h-auto flex flex-col space-y-4 p-4 dark:bg-neutral-950">
-      {parseText(text)}
+    <div className="w-full h-full flex flex-col dark:bg-neutral-950">
+      <JsonToHTML json={json} />
+    </div>
+  )
+}
+
+function JsonToHTML({ json }: { json: LyricNode[] }) {
+  return (
+    <pre className="font-mono">
+      {json.map((section, i) => (
+        <p key={i}>
+          {section.children.map((line, j) => (
+            <LyricLineElement lyricLine={line.children} key={j} />
+          ))}
+        </p>
+      ))}
     </pre>
   )
 }
 
-type LyricNode =
-  | { type: 'line'; children: LyricSegment[] }
-  | { type: 'section'; children: LyricNode[] }
+function LyricLineElement({ lyricLine }: { lyricLine: LyricSegment[] }) {
+  const chords = lyricLine.filter(c => c.type === 'chord')
+
+  return (
+    <div className="font-mono">
+      <div className="flex">
+        {chords.map((chord, i) => {
+          const pad = chords[i - 1]?.pos ?? 0
+          const lenghtLast = chords[i - 1]?.type.length ?? 0
+
+          return (
+            <>
+              <span>
+                {' '.repeat(Math.max(chord.pos - pad - lenghtLast, 0))}
+              </span>
+              <span className="text-red-500">{chord.value}</span>
+            </>
+          )
+        })}
+      </div>
+      <div>
+        {lyricLine.map(lyric => {
+          switch (lyric.type) {
+            case 'label':
+              return <span className="text-gray-500">{lyric.value}</span>
+            case 'text':
+              return <span>{lyric.value}</span>
+          }
+        })}
+      </div>
+    </div>
+  )
+}
+
+function JsonView({ json }: { json: LyricNode[] }) {
+  return (
+    <pre className="w-full h-full overflow-y-scroll flex flex-col space-y-4 p-4 dark:bg-neutral-950">
+      {JSON.stringify(json, null, 2)}
+    </pre>
+  )
+}
+
+type LyricNode = { type: 'section'; children: LyricLine[] }
+
+type LyricLine = { type: 'line'; children: LyricSegment[] }
 
 type LyricSegment =
-  | { type: 'label'; value: string; pos: number }
+  | { type: 'label'; value: string }
   | { type: 'chord'; value: string; pos: number }
-  | { type: 'text'; value: string; pos: number }
+  | { type: 'text'; value: string }
 
 function parseText(text: string) {
   const sections = text.split('\n\n')
@@ -44,31 +119,33 @@ function parseText(text: string) {
 
   for (const section of sections) {
     const lines = section.split('\n')
-    const lineParser: LyricNode[] = lines.map(l => ({
+    const lineParser: LyricLine[] = lines.map(l => ({
       type: 'line',
       children: parseLyricLine(l)
     }))
     songSection.push({ type: 'section', children: lineParser })
   }
 
-  return JSON.stringify(songSection, null, 2)
+  return songSection
 }
 
 const parseLyricLine = (input: string) => {
   const regex = /([A-Z]\.)|\[([^\]]+)\]|([^[\]]+)/g
 
   const segments: LyricSegment[] = []
+  let posBuffer = 0
 
   for (const match of input.matchAll(regex)) {
-    const index = match.index ?? 0
     const [, label, chord, text] = match
 
     if (label) {
-      segments.push({ type: 'label', value: label, pos: index })
+      posBuffer += label.length
+      segments.push({ type: 'label', value: label })
     } else if (chord) {
-      segments.push({ type: 'chord', value: chord, pos: index })
+      segments.push({ type: 'chord', value: chord, pos: posBuffer })
     } else if (text) {
-      segments.push({ type: 'text', value: text, pos: index })
+      posBuffer += text.length
+      segments.push({ type: 'text', value: text })
     }
   }
 
